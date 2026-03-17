69f9003f9bec71cda46af837ef65f3dd02d1ca5f
Asabhiroop20 <abhiroopas2423@gmail.com>
2026-03-18 01:13:57 +0530

    Second Update of Prime num

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -4,6 +4,9 @@ public class CheckPrime {
                 Scanner sc= new Scanner(System.in);
                 int n= sc.nextInt();
                 int temp=0;
+                if(n==1){
+                        temp=1;
+                        }
                 for (int i=2;i<n;i++){
                         if(n%i==0){
                                temp=1;
